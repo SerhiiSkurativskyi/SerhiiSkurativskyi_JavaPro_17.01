ad9c846f3280cadf70f3b701fa8221a8301006f5
Serhii Skurativskyi <serhii.skurativskyi@gmail.com>
2023-01-26 19:00:28 +0200

    changed HW2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import ua.ithillet.lesson2.part1.Car;
 import ua.ithillet.lesson2.part1.Employee;
 import ua.ithillet.lesson2.part2.Cat;
 import ua.ithillet.lesson2.part2.Dog;
@@ -12,17 +13,13 @@ public class Main {
         Employee employee1 = new Employee("Ткачук Петро Іванович", "Фінансовий консультант",
                 "tk.fin@gmail.com", "+38067555555", 35);
 
+        Car car = new Car();
+        car.start();
+
         Dog dog = new Dog("Лорд");
-        dog.run(20);
-        dog.run(500);
-        dog.run(600);
+
         Cat cat = new Cat("Пушок");
-        cat.swim(20);
-        cat.run(20);
-        cat.run(20);
-        cat.run(-20);
-        cat.run(200);
-        cat.run(210);
+
 
         Area[] areas = {new Circle(25),
                 new Rectangle(56, 74),
